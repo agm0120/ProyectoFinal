2396ba70c6a4295ed315bb44ff5ed288290aeac7
agm0120 <agm0120@alu.medac.es>
2026-02-19 16:05:31 +0100

    commit de creacion de los fragment/activitis

--- ProyectoEatQ/app/src/main/java/com/example/proyectoeatq/InformacionHistorialDeporte.java
+++ ProyectoEatQ/app/src/main/java/com/example/proyectoeatq/InformacionHistorialDeporte.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Historial#newInstance} factory method to
+ * Use the {@link InformacionHistorialDeporte#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Historial extends Fragment {
+public class InformacionHistorialDeporte extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Historial extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Historial() {
+    public InformacionHistorialDeporte() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class Historial extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Historial.
+     * @return A new instance of fragment InformacionHistorialDeporte.
      */
     // TODO: Rename and change types and number of parameters
-    public static Historial newInstance(String param1, String param2) {
-        Historial fragment = new Historial();
+    public static InformacionHistorialDeporte newInstance(String param1, String param2) {
+        InformacionHistorialDeporte fragment = new InformacionHistorialDeporte();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Historial extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historial, container, false);
+        return inflater.inflate(R.layout.fragment_informacion_historial_deporte, container, false);
     }
 }
