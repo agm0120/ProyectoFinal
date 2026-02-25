838f73ef5c0c66d5bd1ec22cf81d9ce97b1e0a22
Miguel-Perez621 <mpc0080@alu.medac.es>
2026-02-25 06:42:31 +0100

    estructura mvc

--- ProyectoEatQ/app/src/main/java/com/example/proyectoeatq/view/MainActivity.java
+++ ProyectoEatQ/app/src/main/java/com/example/proyectoeatq/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.proyectoeatq;
+package com.example.proyectoeatq.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.proyectoeatq.R;
 
 public class MainActivity extends AppCompatActivity {
 
