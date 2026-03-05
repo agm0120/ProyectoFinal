0e052c3fa662f59b82855b1e4f52a24c761b321b
Miguel-Perez621 <mpc0080@alu.medac.es>
2026-03-05 09:56:57 +0100

    boton de registro funcionando

--- ProyectoEatQ/app/src/main/java/com/example/proyectoeatq/ActivityMainLogin.java
+++ ProyectoEatQ/app/src/main/java/com/example/proyectoeatq/ActivityMainLogin.java
@@ -2,15 +2,34 @@ package com.example.proyectoeatq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.proyectoeatq.R;
 
 public class ActivityMainLogin extends AppCompatActivity {
 
+    private Button bRegistro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+
+        bRegistro = findViewById(R.id.buttonRegistro);
+
+        bRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent pasarPantalla = new Intent(ActivityMainLogin.this, ActivityRegistroUsuario.class);
+
+                startActivity(pasarPantalla);
+
+            }
+        });
+
     }
 }
