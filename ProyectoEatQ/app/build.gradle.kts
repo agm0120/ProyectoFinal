7c61bb853843f9307b25fc42b22d51004da16b33
Cristina <cam0028@alu.medac.es>
2026-03-13 13:12:49 +0100

    comentarios cambiados

--- ProyectoEatQ/app/build.gradle.kts
+++ ProyectoEatQ/app/build.gradle.kts
@@ -39,14 +39,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    //He añadido el bom de firebase para poder incluir las dependencias de los otros servicios de firebase
-    //La dependencia que habia del auth daba fallo y he tenido que modificarla
+
+    // Firebase BoM. Necesario para incluir las dependenciasde los otros servicios de firebase.
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
-    // TODO: Add the dependencies for Firebase products you want to use
-    // When using de BoM, don't especify versions in Firebase dependencies
-    //Aqui esta la dependencia del auth. Creo que más adelante tendremos que añadir otras dependencias de firebase
+
+    // Firebase dependencies. Dependencia del auth y hueco para otras posibles
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+
+    //Test dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
