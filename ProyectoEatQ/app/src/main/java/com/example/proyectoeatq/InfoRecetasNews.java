8dd0c59eacd8f9430072b1ed2d74c84c304dc3a1
Cristina <cam0028@alu.medac.es>
2026-05-15 13:29:22 +0200

    renombrado xml info plato

--- ProyectoEatQ/app/src/main/java/com/example/proyectoeatq/InfoRecetasNews.java
+++ ProyectoEatQ/app/src/main/java/com/example/proyectoeatq/InfoRecetasNews.java
@@ -35,6 +35,6 @@ public class InfoRecetasNews extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_recetas, container, false);
+        return inflater.inflate(R.layout.fragment_info_metodo_plato, container, false);
     }
 }
